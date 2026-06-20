5132ce7813c28d79a6e0ed90236aea8909a06222
patel-vansh1607 <patel.vansh1607@gmail.com>
2026-06-20 10:59:31 +0300

    updated register

--- src/pages/PublicRegister/PublicRegister.js
+++ src/pages/PublicRegister/PublicRegister.js
@@ -345,11 +345,10 @@ const handleSubmit = async (e) => {
                       <p className={styles.termsHeading}>Terms and Conditions:</p>
                       <ul className={styles.termsList}>
                         <li>Registration is open to children from Grades 1 to 8.</li>
-                        <li>A clear, recent passport-style photograph is required for identification.</li>
                         <li>All information provided must be accurate; incorrect details may delay registration.</li>
-                        <li>Your data is used solely for organizing the Bal-Balika Shibir.</li>
-                        <li>By registering, you consent to receive communication regarding Shibir updates.</li>
-                        <li>By filling this form, you are allowing your child to attend this event, and the organizers are not responsible for any mishaps</li>
+                        <li>Your data is used solely for the Bal-Balika Shibir.</li>
+                        <li>By registering, you consent to receive communication/updates regarding Shibir.</li>
+                        <li>By filling this form, you are allowing your child to attend this event, and the organizers are not responsible for any mishaps.</li>
                       </ul>
                     </div>
                   </div>
