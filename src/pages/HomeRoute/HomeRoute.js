ca5b7cb44d8e606af31931a8a57ba4a5bf60bfa1
patel-vansh1607 <patel.vansh1607@gmail.com>
2026-06-22 12:20:05 +0300

    UPDATING LOGO

--- src/pages/HomeRoute/HomeRoute.js
+++ src/pages/HomeRoute/HomeRoute.js
@@ -1,21 +1,20 @@
 // src/routes/HomeRoute.js
 import React from 'react';
 import styles from './HomeRoute.module.css';
-import { ReactComponent as Logo } from '../../assets/images/Making the Right Choices - Logo_ColorScalable.svg'
+import LogoImage from '../../assets/images/MTRC.png';
 
 const HomeRoute = () => {
   return (
     <div className={styles.container}>
       <section className={styles.brandingSection}>
-        {/* Logo inserted here */}
         <div className={styles.logoContainer}>
-          <Logo className={styles.logo} />
+          <img src={LogoImage} alt="Logo" className={styles.logo} />
         </div>
-
+{/* 
         <header className={styles.publicHeader}>
           <h1>Making the Right Choices</h1>
           <p>Bal-Balika Shibir, Africa - 2026</p>
-        </header>
+        </header> */}
         
         <div className={styles.ctaWrapper}>
           <a href="/register" className={styles.registerBtn}>
