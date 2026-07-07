deaf5a508d31653b768a9324dc4d13682b55069f
patel-vansh1607 <patel.vansh1607@gmail.com>
2026-07-07 22:18:25 +0300

    feat: add additional options for tshirt size selection in PublicRegister

--- src/pages/PublicRegister/PublicRegister.js
+++ src/pages/PublicRegister/PublicRegister.js
@@ -960,6 +960,8 @@ export default function PublicRegister() {
                           <option value="34">34</option>
                           <option value="36">36</option>
                           <option value="38">38</option>
+                          <option value="40">40</option>
+                          <option value="42">42</option>
                         </select>
                       </div>
                     </div>
