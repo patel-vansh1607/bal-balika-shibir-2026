1d07cabd0716ba783b4236bf9e89deab5e4b5c03
patel-vansh1607 <patel.vansh1607@gmail.com>
2026-07-08 23:47:57 +0300

    feat: Update SEVA_DESIGNATIONS to include new categories for better classification

--- src/pages/KarayakarForm/KarayakarForm.js
+++ src/pages/KarayakarForm/KarayakarForm.js
@@ -70,7 +70,7 @@ const regionDataset = {
 const ALL_REGIONS     = Object.keys(regionDataset);
 const TSHIRT_REGIONS  = ['South Africa', 'Botswana','Kenya','Tanzania','Uganda','Malawi','Zambia'];
 const TSHIRT_SIZES    = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
-const SEVA_DESIGNATIONS = ['Shishu Sanchalak', 'Shishu Sah-Sanchalak', 'Shishu I.C','Shishu Helper', 'Shishika Sanchalak', 'Shishika Sah-Sanchalak', 'Shishika I.C','Shishika Helper', 'Bal Sanchalak', 'Bal Sah-Sanchalak', 'Bal I.C','Bal Helper', 'Balika Sanchalak', 'Balika Sah-Sanchalak', 'Balika I.C','Balika Helper'];
+const SEVA_DESIGNATIONS = ['NC','I-NC','NOC', 'I-NOC','RC', 'I-RC','Shishu Sanchalak', 'Shishu Sah-Sanchalak', 'Shishu I.C','Shishu Helper', 'Shishika Sanchalak', 'Shishika Sah-Sanchalak', 'Shishika I.C','Shishika Helper', 'Bal Sanchalak', 'Bal Sah-Sanchalak', 'Bal I.C','Bal Helper', 'Balika Sanchalak', 'Balika Sah-Sanchalak', 'Balika I.C','Balika Helper'];
 
 export default function KarayakarForm() {
   const currentRegionSetting = localStorage.getItem("selected_shibir_region") || "Kenya";
