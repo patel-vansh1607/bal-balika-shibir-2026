4a7acbd31c6a9013fe0be828ee4f44e840d96a0d
patel-vansh1607 <patel.vansh1607@gmail.com>
2026-08-24 12:30:17 +0300

    fix: update supabaseAnonKey to correct value

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://htanzzhgafmeapxhjaux.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0YW56emhnYWZtZWFweGhqYXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMzQ1NjcsImV4cCI6MjA1NjgxMDU2N30.K9J4Y4F1kL6m9N7v8x2Z3v5C6b7N8m9K0l1J2h3G4f5';
-
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0YW56emhnYWZtZWFweGhqYXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1NTgwMDAsImV4cCI6MjEwMzEzNDAwMH0.bbVcfp3pToL1SiTtU5K4IBMerIvzA4EjKrxhNfryEUg';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
